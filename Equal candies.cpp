65b18730165122ab9e688287d7ca9f3e9b01633b
muhtasim-alam-tahsin <muhtasimtahsin258.a@gmail.com>
2025-01-28 18:53:55 +0600

    Add files via upload

---
+++ Equal candies.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;cin>>n;int arr[n+3];
+        long long minimum = INT_MAX;
+        long long sum=0;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];if(arr[i]<minimum) minimum=arr[i];
+        }for(int i=0;i<n;i++){
+            sum += arr[i] - minimum;
+        }cout<<sum<<endl;
+    }return 0;
+}
